0e87c49cf79d9d6f0037be11679cce4e68955045
Shuimomo <shui.zhao@etud.univ-montp2.com>
2016-06-20 18:36:31 +0200

    améliorer structure

--- HilecopMigration/src/migration/MigrationDuProjet.java
+++ HilecopMigration/src/migration/MigrationDuProjet.java
@@ -4,17 +4,32 @@
 package migration;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import hilecopComponent.HilecopComponentDesignFile;
 
 public class MigrationDuProjet {
 	/**
 	 * faire la migration de l'ancien projet vers le nouveau
-	 * @param path1 : chemin de l'ancien projet
-	 * @param path2 : chemin du nouveau projet
+	 * @param ancienfichiers : list de fichiers .hilecopcomponent de l'ancien projet
+	 * @param locate : chemin du nouveau projet
 	 * @throws IOException
 	 */
-	public void migrationHILECOP(String path1, String path2) throws IOException{
+	public void migrationHILECOP(ArrayList<String> ancienfichiers, String locate) throws IOException{
+		int nb = ancienfichiers.size();
+		for(int i=0;i<nb;i++){
+			String path = ancienfichiers.get(i);
+			migrationComposant(path,locate);
+		}
+	}
+	
+	/**
+	 * la migration pour chaque composant
+	 * @param path1
+	 * @param path2
+	 * @throws IOException
+	 */
+	public void migrationComposant(String path1, String path2) throws IOException{
 		ProjetAncien ancien = new ProjetAncien();
 		HilecopComponentDesignFile ancienroot = ancien.read(path1);
 		
